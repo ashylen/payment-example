c23775abebd2a0be1eefc8f27b9cfe35acdf324a
Ashylen <ashylen@o2.pl>
2019-12-08 17:01:26 +0100

    Fixed card view, added getCartTotalCost helper

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,6 +5,7 @@ import classNames from "classnames";
 import { Link, useLocation } from "react-router-dom";
 
 // Utils
+import getTotalCost from "../../helpers/functions/getCartTotalCost";
 import { routes } from "../../routes";
 import styles from "./Cart.module.scss";
 import icoClose from "../../assets/icons/close.svg";
@@ -13,6 +14,7 @@ import icoClose from "../../assets/icons/close.svg";
 
 const Cart = ({ cartItems, isOpen, setIsCartOpen }) => {
   const location = useLocation();
+
   return (
     <React.Fragment>
       <div
@@ -50,6 +52,11 @@ const Cart = ({ cartItems, isOpen, setIsCartOpen }) => {
                             Quantity: {item.quantity}
                           </span>
                         </div>
+                        <div className={styles.quantityWrapper}>
+                          <span className={styles.quantity}>
+                            Price: {item.price}$
+                          </span>
+                        </div>
                       </div>
                     </div>
                   ))}
@@ -57,17 +64,22 @@ const Cart = ({ cartItems, isOpen, setIsCartOpen }) => {
             )}
           </div>
         </div>
-        <div className={styles.bottom}>
-          {location.pathname === routes.checkout ? (
-            <a>
-              <button onClick={() => setIsCartOpen(false)}>Checkout</button>
-            </a>
-          ) : (
-            <Link to={routes.checkout}>
-              <button>Checkout</button>
-            </Link>
-          )}
-        </div>
+        {cartItems && cartItems.length > 0 && (
+          <div className={styles.bottom}>
+            <div className={styles.total}>
+              Total: {getTotalCost(cartItems)}$
+            </div>
+            {location.pathname === routes.checkout ? (
+              <a>
+                <button onClick={() => setIsCartOpen(false)}>Checkout</button>
+              </a>
+            ) : (
+              <Link to={routes.checkout}>
+                <button>Checkout</button>
+              </Link>
+            )}
+          </div>
+        )}
       </div>
     </React.Fragment>
   );
